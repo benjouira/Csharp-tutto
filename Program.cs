85cfebe4d9ffaf0d5b5a710d645313db8930c47f
benjouira <rabi3jouira@gmail.com>
2022-07-08 21:03:39 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string i = "1000";
+                byte b = Convert.ToByte(i);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(":/");
+            }
+            
+        }
+    }
+}
